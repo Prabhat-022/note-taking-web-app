94256f7fb20c7b90d7b2f7b9cc9f33387a57e57f
Prabhat-022 <lograjanand1250@gmail.com>
2025-02-04 21:30:35 +0530

    add cloudinary feature

--- server/src/controllers/note.Controllers.js
+++ server/src/controllers/note.Controllers.js
@@ -1,4 +1,13 @@
 import Note from "../model/note.Model.js"
+import { v2 as cloudinary } from 'cloudinary';
+
+// Configuration
+cloudinary.config({
+    cloud_name: 'dknsgrzbe',
+    api_key: '183245858611542',
+    api_secret: 'L0homrJ7xgFfOzISZNg7QH_0Jv4' // Click 'View API Keys' above to copy your API secret
+});
+
 
 export const createNote = async (req, res) => {
     console.log(req.body)
@@ -11,11 +20,31 @@ export const createNote = async (req, res) => {
                 success: false
             })
         }
+    
+        // Upload audio to Cloudinary
+        const audioUpload = await cloudinary.uploader.upload(audio, {
+            resource_type: "auto"
+        });
+
+        // Upload image to Cloudinary
+        const imageUpload = await cloudinary.uploader.upload(image, {
+            resource_type: "auto"
+        });
+
+        if (!audioUpload || !imageUpload) {
+            return res.status(400).json({
+                message: "Failed to upload audio or image on cloudinary",
+                success: false
+            })
+        }
+
+        console.log('audioUpload:', audioUpload)
+        console.log('imageUpload:', imageUpload)
 
         const note = await Note.create({
-            audio,
+            audio: audioUpload?.url,
             text,
-            image,
+            image: imageUpload?.url,
             user
         })
 
@@ -24,6 +53,7 @@ export const createNote = async (req, res) => {
             success: true,
             data: note
         })
+
     } catch (error) {
         console.log(`Error creating note: ${error}`)
         return res.status(500).json({
